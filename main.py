5f9042f9030e5aa76095e493a011fd792d3c93df
Roberto Tyley <roberto.tyley@gmail.com>
2015-11-18 13:39:41 +0000

    try to rx the thing

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python2.7
 # See also http://RasPi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
-import RPi.GPIO as GPIO
+from __future__ import print_function
 
 import os
 import subprocess
-from time import sleep
+
+import RPi.GPIO as GPIO
+from rx.subjects import Subject
 
 GPIO.setmode(GPIO.BCM)
 
@@ -16,17 +18,19 @@ GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # this will run in another thread when our events are detected
 def octoalert_pressed(channel):
-    print "OctoAlert pressed!"
+    print("OctoAlert pressed!")
     os.system('omxplayer /home/pi/OctoAlert.AnderDiningRoom.mp3 &')
 
 # raw_input("Press Enter when ready\n>")
 
 def time_needs_sync():
-	return subprocess.check_output(['ntpq', '-c', 'rv 0 reftime']).startswith('reftime=0000')
+    return subprocess.check_output(['ntpq', '-c', 'rv 0 reftime']).startswith('reftime=0000')
 
-GPIO.add_event_detect(25, GPIO.FALLING, callback=octoalert_pressed, bouncetime=500)
+buttonPressSubject = Subject()
 
-print time_needs_sync()
+GPIO.add_event_detect(25, GPIO.FALLING, callback=buttonPressSubject.on_next, bouncetime=500)
+
+buttonPressSubscription = buttonPressSubject.subscribe(lambda x: print("Got: %s" % x))
 
 import time
 from time import strftime
@@ -38,24 +42,23 @@ display = AlphaNum4.AlphaNum4()
 display.begin()
 
 pos = 0
-print 'Press Ctrl-C to quit.'
+
 while True:
-        display.clear()
-        display.set_brightness(0)
+    display.clear()
+    display.set_brightness(0)
 
-        timeText = strftime("%H%M")
+    timeText = strftime("%H%M")
 
+    if time_needs_sync():
+        display.set_decimal(pos % 4, True)
+    else:
+        display.print_str(timeText)
+        display.set_decimal(1, pos % 2)
 
-	if time_needs_sync():
-	        display.set_decimal(pos % 4, True)
-	else:
-	        display.print_str(timeText)
-	        display.set_decimal(1, pos % 2)
+    display.write_display()
 
-        display.write_display()
-
-        pos += 1
-        time.sleep(1)
+    pos += 1
+    time.sleep(1)
 
 GPIO.cleanup()           # clean up GPIO on normal exit
-
+buttonPressSubscription.dispose()
